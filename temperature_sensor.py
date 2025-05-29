2cdf7d01042e87d31b8ee72c381e3323f985a487
Durga Lakshmi Sajith <durgasaj000@gmail.com>
2025-05-29 21:30:30 +0200

    test push durga

--- temperature_sensor.py
+++ temperature_sensor.py
@@ -7,3 +7,5 @@ while True:
     [temperature,humidity] = grovepi.dht(port,sensor)
     print ("{} Temperature = {} C, humidity = {} %".format(time.ctime(),temperature,humidity))
     time.sleep(timeout)
+
+#test push durga
