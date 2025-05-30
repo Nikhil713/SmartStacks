fa362728a1354d810be7cbbd10f3b6de7b1c6b64
Nikhil Babu <nikhilbabu213@gmail.com>
2025-05-30 22:18:03 +0200

    Enable only LDR and temp

--- main.py
+++ main.py
@@ -6,28 +6,28 @@ import sys
 
 # ---- Parallel Process Launch ---- #
 if __name__ == '__main__':
-    # p1 = Process(target=hU.ldr_led)
+    p1 = Process(target=hU.ldr_led)
     # p2 = Process(target=hU.sound_oled)
     p3 = Process(target=hU.run_temperature_control_loop)
     # p3 = Process(target=read_light)
 
-    # p1.start()
+    p1.start()
     # p2.start()
     p3.start()
     # p3.start()
 
     try:
-        # p1.join()
+        p1.join()
         # p2.join()
         p3.join()
         # p3.join()
 
     except KeyboardInterrupt:
         print("Main process interrupted - Terminating child process")
-        # p1.terminate()
+        p1.terminate()
         # p2.terminate()
         p3.terminate()
-        # p1.join()
+        p1.join()
         # p2.join()
         p3.join()
         sys.exit(0)
