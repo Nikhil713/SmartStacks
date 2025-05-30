44d9a803dc85cea476bd04d968eaad9641b13cd3
Swathi M S <Swathi0712>
2025-05-30 23:06:43 +0200

    Change to multi threading

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from multiprocessing import Process
+# from multiprocessing import Process
+from threading import Thread
 import hardwareUtilities as hU
 import sys
 #import schedule
@@ -9,30 +10,35 @@ if __name__ == '__main__':
     # p1 = Process(target=hU.ldr_led)
     # p2 = Process(target=hU.sound_oled)
     # p3 = Process(target=hU.run_temperature_control_loop)
-    p4 = Process(target=hU.run_pir_monitor_loop)
+    # p4 = Process(target=hU.run_pir_monitor_loop)
+
+    p1 = Thread(target=hU.ldr_led)
+    p2 = Thread(target=hU.sound_oled)
+    p3 = Thread(target=hU.run_temperature_control_loop)
+    p4 = Thread(target=hU.run_pir_monitor_loop)
     # p3 = Process(target=read_light)
 
-    # p1.start()
-    # p2.start()
-    # p3.start()
+    p1.start()
+    p2.start()
+    p3.start()
     p4.start()
 
     try:
-        # p1.join()
-        # p2.join()
-        # p3.join()
+        p1.join()
+        p2.join()
+        p3.join()
         p4.join()
 
     except KeyboardInterrupt:
         print("Main process interrupted - Terminating child process")
+        Thread.stop_event.set()  # Signal all threads to stop
         # p1.terminate()
         # p2.terminate()
         # p3.terminate()
-        p4.terminate()
+        # p4.terminate()
         # p1.join()
         # p2.join()
         # p3.join()
-        p4.join()
         sys.exit(0)
         set_fan_speed(0)  # Turn off fan on exit
 
