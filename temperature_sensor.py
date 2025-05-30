452fa25af16b2ec4abf2eb3c5b3bfe4c8ba8a7fa
Nikhil Babu <nikhilbabu213@gmail.com>
2025-05-30 21:03:17 +0200

    Remove temp sensor

--- temperature_sensor.py
+++
@@ -1,9 +0,0 @@
-import grovepi
-import time
-port=4
-sensor=0
-timeout = 5
-while True:
-    [temperature,humidity] = grovepi.dht(port,sensor)
-    print ("{} Temperature = {} C, humidity = {} %".format(time.ctime(),temperature,humidity))
-    time.sleep(timeout)
