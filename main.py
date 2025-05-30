6c4450779fc2965605880790ba947ff920c337c9
Nikhil Babu <nikhilbabu213@gmail.com>
2025-05-30 21:58:57 +0200

    Only sound

--- main.py
+++ main.py
@@ -11,25 +11,25 @@ if __name__ == '__main__':
     p3 = Process(target=hU.run_temperature_control_loop)
     # p3 = Process(target=read_light)
 
-    p1.start()
+    # p1.start()
     p2.start()
-    p3.start()
+    # p3.start()
     # p3.start()
 
     try:
-        p1.join()
+        # p1.join()
         p2.join()
-        p3.join()
+        # p3.join()
         # p3.join()
 
     except KeyboardInterrupt:
         print("Main process interrupted - Terminating child process")
-        p1.terminate()
+        # p1.terminate()
         p2.terminate()
-        p3.terminate()
-        p1.join()
+        # p3.terminate()
+        # p1.join()
         p2.join()
-        p3.join()
+        # p3.join()
         sys.exit(0)
         set_fan_speed(0)  # Turn off fan on exit
 
