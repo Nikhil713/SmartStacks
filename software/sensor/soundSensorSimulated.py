5fb6fdee2e7d14259859f9ce4039766be3022ed3
Swathi0712 <swathimangala82@gmail.com>
2025-06-01 18:10:06 +0200

    Update utilities

--- software/sensor/soundSensorSimulated.py
+++ software/sensor/soundSensorSimulated.py
@@ -11,6 +11,7 @@ def get_random_sound_value():
     try:
         sensor_value = random.randint(0, 1023)
         print(sensor_value)
+        
         if sensor_value < 100:
             level = "Quiet"
         elif sensor_value < 200:
---
+++ softwareUtilities.py
@@ -0,0 +1,31 @@
+import time
+from logger import log
+from datetime import datetime
+from software.sensor.soundSensorSimulated import noiseLevel
+from hardware.actuator.LCD_Display import *
+
+
+def sound_lcd():
+    try:
+        while True:
+            print("sensor_value in hu")
+            # Sound Sensor + OLED
+            sound_value, sound_level = noiseLevel()
+            print(sound_value)
+            msg = f"Sound: {sound_level}"
+            setRGB(0,128,64)
+            setText(msg)
+            setText(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            log(msg)
+            time.sleep(5)
+            # for c in range(0,255):
+            #     setText_norefresh("Going to sleep in {}...".format(str(c)))
+            #     setRGB(c,255-c,0)
+            #     time.sleep(0.1)
+            setRGB(255,0,0)
+            print(f"[Sound] Raw: {sound_value}, Level: {sound_level}")
+            # setText("Bye bye, this should wrap onto next line")
+            # time.sleep(5)
+
+    except KeyboardInterrupt:
+        print("Sound-OLED process interrupted")
