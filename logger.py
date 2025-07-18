c811830f21588d5e73a52ccc51df74306802e0c3
Swathi Mangala Suresh <swathimangala82@gmail.com>
2025-07-19 00:54:28 +0200

    Log update

--- logger.py
+++ logger.py
@@ -2,10 +2,15 @@ from datetime import datetime
 import paho.mqtt.client as mqtt
 
 # MQTT configuration
-MQTT_BROKER = "mqtt.example.com"
+MQTT_BROKER = "test.mosquitto.org"
 MQTT_PORT = 1883
 MQTT_TOPIC = "device/logs"
+CLIENT_ID = "raspberrypi_sensor_01" 
 
+
+# Setup MQTT client once
+mqtt_client = mqtt.Client(client_id=CLIENT_ID)
+mqtt_client.connect(MQTT_BROKER, MQTT_PORT)
 
 LOG_FILE = "device_log.txt"  # Log file 
 
